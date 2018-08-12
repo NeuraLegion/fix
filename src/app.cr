81bd9893f745fb77656a5882b80d0bcb3e0bdcd3
Dekel <dekel1.reches.gmail.com>
2018-08-12 14:55:20 +0300

    app docs

--- src/app.cr
+++ src/app.cr
@@ -2,11 +2,24 @@ require "./message"
 require "./exception"
 
 abstract class FIXApplication
+  # Called when succesful logon takes place
   abstract def on_logon
+
+  # Called when session ends, either by logout or disconnection
   abstract def on_logout
+
+  # Called when an administrative/session message is about to be sent, throw `DoNotSend` to not send
   abstract def to_admin(msg : FIXMessage)
+
+  # Called when an application message is about to be sent, throw `DoNotSend` to not send
   abstract def to_app(msg : FIXMessage)
+
+  # Called when an administrative/session message is received
   abstract def from_admin(msg : FIXMessage)
+
+  # Called when an application message is received
   abstract def from_app(msg : FIXMessage)
+
+  # Called when an error occurs ( Session or message decoding issues )
   abstract def on_error(err : FIXException)
 end
