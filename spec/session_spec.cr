a86477ac675bdaf652c8398c275388efff85f38d
Bar Hofesh <bar.hofesh@gmail.com>
2018-08-14 14:41:49 +0300

    fixed specs

--- spec/session_spec.cr
+++ spec/session_spec.cr
@@ -2,7 +2,9 @@ require "./spec_helper"
 
 describe FIX::Session do
   it "initialize" do
-    session = FIX::Session.new
+    serv = TCPServer.new(9898)
+    session = FIX::Session.new "localhost", 9898
     session.should be_a(FIX::Session)
+    serv.close
   end
 end
